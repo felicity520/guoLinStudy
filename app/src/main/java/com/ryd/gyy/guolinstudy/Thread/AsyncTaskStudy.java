492ade86375c815c69c6062c42a06195005910e1
felicity520 <3076633801@qq.com>
2019-10-17 21:18:48 +0800

    20191017提交AndroidManifest.xml

---
+++ app/src/main/java/com/ryd/gyy/guolinstudy/Thread/AsyncTaskStudy.java
@@ -0,0 +1,135 @@
+package com.ryd.gyy.guolinstudy.Thread;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.ryd.gyy.guolinstudy.R;
+
+/**
+ * 参考：https://www.jianshu.com/p/37502bbbb25a
+ * 异步消息处理机制 Thread + handler
+ * AsyncTaskStudy就是将具体的方法封装好，比如在哪个方法更新UI，在哪个方法进行耗时操作等等
+ */
+public class AsyncTaskStudy extends AppCompatActivity implements View.OnClickListener {
+
+    // 线程变量
+    MyTask mTask;
+
+    // 主布局中的UI组件
+    Button button, cancel; // 加载、取消按钮
+    TextView text; // 更新的UI组件
+    ProgressBar progressBar; // 进度条
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_synctask);
+
+        initView();
+
+
+    }
+
+    private void initView() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        cancel = (Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(this);
+
+        text = (TextView) findViewById(R.id.text);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                mTask = new MyTask();
+                mTask.execute();
+                break;
+            case R.id.cancel:
+                // 取消一个正在执行的任务,调用cancel之后onCancelled方法将会被调用
+                mTask.cancel(true);
+                break;
+        }
+    }
+
+    /**
+     * 步骤1：创建AsyncTask子类
+     * 注：
+     * a. 继承AsyncTask类
+     * b. 为3个泛型参数指定类型；若不使用，可用java.lang.Void类型代替
+     * 此处指定为：输入参数 = String类型、执行进度 = Integer类型、执行结果 = String类型
+     * c. 根据需求，在AsyncTask子类内实现核心方法
+     */
+    private class  MyTask extends AsyncTask<String, Integer, String> {
+
+        // 方法1：onPreExecute（）
+        // 作用：执行 线程任务前的操作
+        @Override
+        protected void onPreExecute() {
+            text.setText("加载中");
+            // 执行前显示提示
+        }
+
+
+        // 方法2：doInBackground（）
+        // 作用：接收输入参数、执行任务中的耗时操作、返回 线程任务执行的结果
+        // 此处通过计算从而模拟“加载进度”的情况
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                int count = 0;
+                int length = 1;
+                while (count < 99) {
+
+                    count += length;
+                    // 可调用publishProgress（）显示进度, 之后将执行onProgressUpdate（）
+                    publishProgress(count);
+                    // 模拟耗时任务
+                    Thread.sleep(50);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        // 方法3：onProgressUpdate（）
+        // 作用：在主线程 显示线程任务执行的进度
+        @Override
+        protected void onProgressUpdate(Integer... progresses) {
+
+            progressBar.setProgress(progresses[0]);
+            text.setText("loading..." + progresses[0] + "%");
+
+        }
+
+        // 方法4：onPostExecute（）
+        // 作用：接收线程任务执行结果、将执行结果显示到UI组件
+        @Override
+        protected void onPostExecute(String result) {
+            // 执行完毕后，则更新UI
+            text.setText("加载完毕");
+        }
+
+        // 方法5：onCancelled()
+        // 作用：将异步任务设置为：取消状态
+        @Override
+        protected void onCancelled() {
+
+            text.setText("已取消");
+            progressBar.setProgress(0);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/ryd/gyy/guolinstudy/Thread/ThreadStudy.java
@@ -0,0 +1,167 @@
+package com.ryd.gyy.guolinstudy.Thread;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.ryd.gyy.guolinstudy.R;
+
+
+/**
+ * 参考：https://www.jianshu.com/p/49349eee9abc
+ */
+public class ThreadStudy extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "ThreadStudy";
+
+    public volatile boolean stop = false;
+
+    MyThread mMyThread;
+    private Button threadStart;
+    private Button threadStop;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+
+        initView();
+
+//        thirdKinds();
+    }
+
+    private void initView() {
+        threadStart = (Button) findViewById(R.id.threadStart);
+        threadStart.setOnClickListener(this);
+
+        threadStop = (Button) findViewById(R.id.threadStop);
+        threadStop.setOnClickListener(this);
+    }
+
+
+    private void firstKind() {
+        mMyThread = new MyThread();
+        mMyThread.start();
+    }
+
+    private void secondKind() {
+        MyRunnable MyRunnable = new MyRunnable();
+        new Thread(MyRunnable).start();
+    }
+
+    /**
+     * 第三种方式：不用专门像第二种方法实现Runnable接口，这里直接使用匿名内部类的方式
+     */
+    private void thirdKinds() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                //使用标志位来终止线程
+//                int i = 0;
+//                while (!stop) {
+//                    // thread runing
+//                    Log.v(TAG, "gyy run: " + i++);
+//                    stop = true;
+//                }
+
+
+            }
+        }).start();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.threadStart:
+                firstKind();
+                break;
+            case R.id.threadStop:
+                stopThread();
+                break;
+        }
+    }
+
+
+    /**
+     * interrupt()中断线程
+     */
+    private void stopThread() {
+        Log.e(TAG, "mMyThread: " + mMyThread );
+        Log.e(TAG, "mMyThread.isAlive(): " + mMyThread.isAlive() );
+        if (null != mMyThread && mMyThread.isAlive()) {
+            Log.e(TAG, "stopThread: -----");
+            mMyThread.interrupt();
+            mMyThread = null;
+        }
+    }
+
+
+    /**
+     * 第一种：继承线程
+     */
+    public class MyThread extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+            //针对线程正常运行状态
+//            int i = 0;
+//            // 判断状态，如果被打断则跳出并将线程置空
+//            while (!isInterrupted()) {
+//                i++;
+//                Log.i(TAG, "线程名字" + Thread.currentThread().getName() + "  Count i:" + i);
+//            }
+
+
+            //针对线程阻塞状态
+//            int i = 0;
+//            while(true){
+//                try {
+//                    i++;
+//                    Thread.sleep(1000);
+//                    Log.i(TAG,Thread.currentThread().getName()+":Running()_Count:"+i);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                    Log.i(TAG,Thread.currentThread().getName()+"异常抛出，停止线程");
+//                    break;
+//                }
+//            }
+
+            //终极方法
+            int i = 0;
+            while(!isInterrupted()){  // 判断线程是否被打断
+                try {
+                    i++;
+                    Thread.sleep(1000);
+                    Log.i("thread",Thread.currentThread().getName()+":Running()_Count:"+i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Log.i("thread",Thread.currentThread().getName()+"异常抛出，停止线程");
+                    break;// 抛出异常跳出循环
+                }
+            }
+
+
+        }
+    }
+
+
+    /**
+     * 第二种：实现Runnable
+     */
+    public class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            // do something
+        }
+    }
+
+
+
+}
