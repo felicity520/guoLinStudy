84d88d3367ffa159ba1994bb54aeec2752c3ea5c
felicity520 <51052978+felicity520@users.noreply.github.com>
2019-11-19 20:01:32 +0800

    20191119家里尝试push到gitee

--- app/src/main/java/com/ryd/gyy/guolinstudy/Activity/MainActivity.java
+++ app/src/main/java/com/ryd/gyy/guolinstudy/Activity/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
