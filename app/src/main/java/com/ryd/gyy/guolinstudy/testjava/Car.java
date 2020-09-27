29ce84d46ab295a91af9d04da9972fa4c8b63b41
felicity520 <51052978+felicity520@users.noreply.github.com>
2020-09-27 22:56:29 +0800

    学习反射

--- app/src/main/java/com/ryd/gyy/guolinstudy/testjava/Car.java
+++ app/src/main/java/com/ryd/gyy/guolinstudy/testjava/Car.java
@@ -1,10 +1,17 @@
 package com.ryd.gyy.guolinstudy.testjava;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import static java.sql.DriverManager.println;
 
 /***
+ * 通过反射运行配置文件内容
+ *
  * 原型模式：https://www.jianshu.com/p/465c25491eaf
  * 浅拷贝的时候String等基本类型数据是不会改变的，ArrayList<String>这样的引用类型来说，原始的数据也会改变。
  */
@@ -47,8 +54,73 @@ public class Car implements Cloneable {
         //再打印一遍
         println("car_____:" + car.hashCode() + "__集合大小:" + car.wheelList.size());
         println("cloneCar_____:" + cloneCar.hashCode() + "__集合大小:" + cloneCar.wheelList.size());
+
+
+        //通过反射获取Class对象
+        Class stuClass = null;//"cn.fanshe.Student"
+        try {
+            stuClass = Class.forName(getValue("className"));
+            //2获取show()方法
+            Method m = stuClass.getMethod(getValue("methodName"));//show
+            //3.调用show()方法
+            m.invoke(stuClass.getConstructor().newInstance());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+
+        /*
+         * 通过反射越过泛型检查
+         *
+         * 例如：有一个String泛型的集合，怎样能向这个集合中添加一个Integer类型的值？
+         */
+        try {
+            ArrayList<String> strList = new ArrayList<>();
+            strList.add("aaa");
+            strList.add("bbb");
+
+            //	strList.add(100);
+            //获取ArrayList的Class对象，反向的调用add()方法，添加数据
+            Class listClass = strList.getClass(); //得到 strList 对象的字节码 对象
+            //获取add()方法
+            Method m = null;
+            m = listClass.getMethod("add", Object.class);
+            //调用add()方法
+            m.invoke(strList, 100);
+            //遍历集合
+            for (Object obj : strList) {
+                System.out.println(obj);
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
+    //此方法接收一个key，在配置文件中获取相应的value
+    public static String getValue(String key) throws IOException {
+        Properties pro = new Properties();//获取配置文件的对象
+        FileReader in = new FileReader("C:\\Users\\ADMIN\\Desktop\\pro.txt");//获取输入流
+        pro.load(in);//将流加载到配置文件对象中
+        in.close();
+        return pro.getProperty(key);//返回根据key获取的value值
+    }
 
 }
 
