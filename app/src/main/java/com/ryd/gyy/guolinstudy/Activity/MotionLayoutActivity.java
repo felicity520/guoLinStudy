fcdb35601c3a9e3507d64f75a2389a611aa35b44
felicity520 <3076633801@qq.com>
2020-11-18 17:47:43 +0800

    提交MotionLayout加入关键帧

--- app/src/main/java/com/ryd/gyy/guolinstudy/Activity/MotionLayoutActivity.java
+++ app/src/main/java/com/ryd/gyy/guolinstudy/Activity/MotionLayoutActivity.java
@@ -11,7 +11,13 @@ public class MotionLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //直线滑动的
 //        setContentView(R.layout.activity_motion_layout);
-        setContentView(R.layout.activity_motion_layout1);
+        //三个图滑动的
+//        setContentView(R.layout.activity_motion_layout1);
+        //小风车的
+//        setContentView(R.layout.activity_motion_sample_step3);
+        //很多篮球的动画
+        setContentView(R.layout.activity_multi_ball_layout);
     }
 }
