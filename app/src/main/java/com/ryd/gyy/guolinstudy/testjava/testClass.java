cac8373fdb43e8a2e699a03571808696a508d371
felicity520 <3076633801@qq.com>
2020-12-24 09:36:30 +0800

    测试classloader

--- app/src/main/java/com/ryd/gyy/guolinstudy/testjava/testClass.java
+++ app/src/main/java/com/ryd/gyy/guolinstudy/testjava/testClass.java
@@ -1,4 +1,65 @@
 package com.ryd.gyy.guolinstudy.testjava;
 
 public class testClass {
+
+    public static void main(String[] args) {
+        Son s1 = new Son();
+        System.out.println("gyy----");
+        Son s2 = new Son();
+    }
+
+    public static class Father {
+        private int i = test();
+        private static int j = method();
+
+        static {
+            System.out.println("(1)");
+        }
+
+        Father() {
+            System.out.println("(2)");
+        }
+
+        {
+            System.out.println("(3)");
+        }
+
+        public int test() {
+            System.out.println("(4)");
+            return 1;
+        }
+
+        public static int method() {
+            System.out.println("(5)");
+            return 1;
+        }
+    }
+
+    public static class Son extends Father {
+        private int i = test();
+        private static int j = method();
+
+        static {
+            System.out.println("(6)");
+        }
+
+        Son() {
+            System.out.println("(7)");
+        }
+
+        {
+            System.out.println("(8)");
+        }
+
+        public int test() {
+            System.out.println("(9)");
+            return 1;
+        }
+
+        public static int method() {
+            System.out.println("(10)");
+            return 1;
+        }
+    }
+
 }
