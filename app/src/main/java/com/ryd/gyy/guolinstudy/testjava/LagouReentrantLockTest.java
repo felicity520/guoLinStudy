38e137242c44daca88afe630f9d46d9c059ee996
felicity520 <51052978+felicity520@users.noreply.github.com>
2021-01-10 22:54:08 +0800

    学习GC

--- app/src/main/java/com/ryd/gyy/guolinstudy/testjava/LagouReentrantLockTest.java
+++ app/src/main/java/com/ryd/gyy/guolinstudy/testjava/LagouReentrantLockTest.java
@@ -39,7 +39,7 @@ public class LagouReentrantLockTest {
             //手动解锁
             //说明：在上面 ReentrantLock 的使用中，我将 unlock 操作放在 finally 代码块中。
             // 这是因为 ReentrantLock 与 synchronized 不同，当异常发生时 synchronized 会自动释放锁，
-            // 但是 ReentrantLock 并不会自动释放锁。因此好的方式是将 unlock 操作放在 finally 代码块中，保证任何时候锁都能够被正常释放掉。 
+            // 但是 ReentrantLock 并不会自动释放锁。因此好的方式是将 unlock 操作放在 finally 代码块中，保证任何时候锁都能够被正常释放掉。
             lock.unlock();
         }
     }
